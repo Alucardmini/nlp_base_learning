499c6dff8f4494812357536e6ee83e875206398f
wuxikun <linuxjade@163.com>
2018-11-28 16:01:21 +0800

    增加新词发现功能

---
+++ find_new_word/src/Main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+@author: wuxikun
+@contact: wuxikun@bjgoodwill.com
+@software: PyCharm Community Edition
+@file: Main.py
+@time: 11/28/18 3:59 PM
+"""
+
+
+class Findnewwords(object):
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
