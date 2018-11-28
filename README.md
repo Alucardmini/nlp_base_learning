87a8721bc2b75b6057dfbbe3f7dcde3878e023ef
wuxikun <linuxjade@163.com>
2018-11-28 16:44:54 +0800

    add stop wordslists

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # nlp_base_learning
-learing NLP
+learing NLP step by step
 ### 2018-11-28 完成文本情感分类工程
+
+### 2018-11-28 增加新词发现工程
--- find_new_word/src/Main.py
+++ find_new_word/src/Main.py
@@ -8,12 +8,23 @@
 @file: Main.py
 @time: 11/28/18 3:59 PM
 """
-
+import pandas as pd
+import numpy as np
 
 class Findnewwords(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, data_path, min_count=10, max_step=4):
+        self.max_step = max_step
+        self.min_count = min_count
+        self.src_content_list = self.loaddatafromPath(data_path)
+
+    def loaddatafromPath(self, data_path):
+        with open(data_path, 'r')as f:
+            src_content = ''.join(f.readlines())
+        src_content_list = list(src_content)
+
+        src_count_list = pd.Series(src_content_list).value_counts()
+        return src_content_list
 
 
 if __name__ == '__main__':
